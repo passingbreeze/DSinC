0e175492121d55221cd3b562921cce1e1e39c7d1
passingbreeze <jungmin1237@pusan.ac.kr>
2018-12-09 02:19:30 +0900

    doubly circular linked list making...

--- list.c
+++ list.c
@@ -13,14 +13,14 @@ typedef struct list {
     int data;
     struct list *prev;
     struct list *next;
-    bool ishead;
 } list;
 
 void insert(list**, int, int);
 list* find(list**, int);
 void erase(list**, int);
 void showlist(list**);
-
+int size(list**);
+int at(list**, int);
 
 // to this line.
 
@@ -51,12 +51,93 @@ int main(int argc, char *argv[])
                 else
                     printf("Not found...\n");
             case DEL:
-
+                printf("what number do you wanna delete? >> ");
+                scanf("%d", &value);
+                erase(&root, value);
             case PRINT:
+                printf("%d elements are in list.\n In list -> ", size(&root));
+                showlist(&root);
+                printf("\n");
             default:
                 printf("\nPlease input the number between 1 and 6\n");
                 break;
         }
     }
     return EXIT_SUCCESS;
+}
+
+int size(list** root)
+{
+    int count=1;
+    if((*root)==nullptr)
+        return count;
+    else {
+        while((*root)->next != nullptr){
+            (*root) = (*root)->next;
+            count++;
+        }
+    }
+    return count;
+}
+
+int at(list** root, int i)
+{
+    int count=0;
+    if((*root)==nullptr || i > size(root)){
+        fputs("out of range", stderr);
+        exit(1);
+    }
+    else {
+        if(i==count)
+            return (*root)->data;
+        else{
+            while(count < i){
+                (*root) = (*root) -> next;
+                count++;
+            }
+            return (*root)->data;
+        }
+    }
+}
+
+list* find(list **root, int v)
+{
+    if((*root) == nullptr){
+        return nullptr;
+    }
+    else {
+        list *p = (*root)->prev;
+        list *n = (*root)->next;
+    }
+}
+
+void insert(list **root, int kth, int v)
+{
+    if((*root)==nullptr){
+        (*root) = (list*)malloc(sizeof(list));
+        (*root)->data = v;
+        (*root)->prev = nullptr;
+        (*root)->next = nullptr;
+    }
+    else {
+        list* new = (list*)malloc(sizeof(list));
+        new->data = v;
+        if(kth >= size(root)){
+            (*root)->next = new;
+            new->prev = *root;
+            new->next = nullptr;
+        }
+        else {
+            new->next = (*root)->next;
+            (*root)->next->prev = new;
+            new->prev = *root;
+            (*root)->next = new;
+        }
+    }
+
+}
+
+void erase(list **root, int v)
+{
+
 }
