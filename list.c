97e7eaf9cc5adc2bf8d16f029ac2c23b7410a195
passingbreeze <jungmin1237@pusan.ac.kr>
2018-12-09 19:33:08 +0900

    doubly circular linked list modifying insert function...

--- list.c
+++ list.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #define nullptr ((void*)0)
 
@@ -13,6 +14,7 @@ typedef struct list {
     int data;
     struct list *prev;
     struct list *next;
+    bool ishead, istail;
 } list;
 
 bool isempty(list**);
@@ -22,7 +24,7 @@ void erase(list**, int);
 void showlist(list**);
 void clear(list**);
 int size(list**);
-int at(list**, int);
+list* at(list**, int);
 
 // to this line.
 
@@ -33,20 +35,21 @@ int main(int argc, char *argv[])
     int select=0, kth=0, value=0;
     while(true){
         printf("\nLet's make List!\n");
-        printf("1. INSERT | 2. FIND SOMETHING | 3. DELETE SOMETHING | 4. SHOW LIST | 5. STOP MANIPULATE LIST | 6. QUIT PROGRAM\n");
+        printf("1. INSERT | 2. FIND SOMETHING | 3. DELETE SOMETHING | 4. SHOW LIST | 5. STOP MANIPULATE LIST | 6. QUIT PROGRAM\n >> ");
         scanf("%d", &select);
         if(select == STOP)
             break;
         if(select == QUIT) {
             printf("\nProgram will be terminated...\n");
             clear(&root);
-            exit(0);
+            exit(EXIT_SUCCESS);
         }
         switch(select){
             case INSERT:
                 printf("Enter input data and \'k\'th where you wanna input from head >>  ");
-                scanf("%d%d", &value, &kth);
+                scanf("%d%d", &kth, &value);
                 insert(&root, kth, value);
+                break;
             case FIND:
                 printf("what number do you wanna find? >> ");
                 scanf("%d", &value);
@@ -54,14 +57,21 @@ int main(int argc, char *argv[])
                     printf("%d is in list!\n", find(&root,value)->data);
                 else
                     printf("Not found...\n");
+                break;
             case DEL:
                 printf("what number do you wanna delete? >> ");
                 scanf("%d", &value);
                 erase(&root, value);
+                break;
             case PRINT:
-                printf("%d elements are in list.\n In list -> ", size(&root));
-                showlist(&root);
-                printf("\n");
+                if(!size(&root))
+                    printf("No Elements in list!\n");
+                else {
+                    printf("%d elements are in list.\n In list : ", size(&root));
+                    showlist(&root);
+                    printf("\n");
+                }
+                break;
             default:
                 printf("\nPlease input the number between 1 and 6\n");
                 break;
@@ -74,11 +84,14 @@ int main(int argc, char *argv[])
 int size(list** root)
 {
     int count=1;
-    if((*root)==nullptr)
+    list *temp = (*root);
+    if(temp == nullptr)
         return 0;
+    else if(temp->next == temp)
+        return 1;
     else {
-        while((*root)->next != nullptr){
-            (*root) = (*root)->next;
+        while(!temp->next->istail){
+            temp = temp->next;
             count++;
         }
     }
@@ -93,28 +106,28 @@ bool isempty(list **root)
 void clear(list **root)
 {
     list *temp;
-    while((*root)!=nullptr){
+    while(!(*root)->istail){
         temp = (*root);
         (*root) = (*root)->next;
         free(temp);
     }
-    *root = nullptr;
+    free(*root);
 }
 
 
-int at(list** root, int i)
+list* at(list** root, int i)
 {
     int count=0;
     if(isempty(root) || i > size(root)){
         fputs("out of range", stderr);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     else {
         while(count < i){
             (*root) = (*root) -> next;
             count++;
         }
-        return (*root)->data;
+        return *root;
     }
 }
 
@@ -134,31 +147,42 @@ list* find(list **root, int v)
 
 void insert(list **root, int kth, int v)
 {
+    list* new = (list*)malloc(sizeof(list));
+    list* temp = *root;
     if(isempty(root)){
+        printf("list begin!\n");
         (*root) = (list*)malloc(sizeof(list));
         (*root)->data = v;
-        (*root)->prev = nullptr;
-        (*root)->next = nullptr;
+        (*root)->prev = (*root)->next = *root;
+        (*root)->ishead = true;
+        (*root)->istail = false;
     }
     else {
-        list* new = (list*)malloc(sizeof(list));
-        list* temp = (*root);
         new->data = v;
-        if(kth >= size(root)){
-            while(temp->next != nullptr)
+        new->ishead = false;
+        new->istail = true;
+        if(size(root)==1){
+            (*root)->prev = (*root)->next = new;
+            new->prev = new->next = *root;
+        }
+        else if(kth < size(root)){
+            temp = at(root, kth);
+            new->istail = false;
+            temp->next = new;
+            new->prev = temp;
+            new->next = temp->next;
+            temp->next->prev = new;
+        }
+        else {
+            while(!temp->istail)
                 temp = temp->next;
             temp->next = new;
             new->prev = temp;
             new->next = (*root);
-        }
-        else {
-            new->next = (*root)->next;
-            (*root)->next->prev = new;
-            new->prev = *root;
-            (*root)->next = new;
+            (*root)->prev = new;
         }
     }
-
+    printf("%d elements in list\n", size(root));
 }
 
 void erase(list **root, int v)
@@ -169,6 +193,17 @@ void erase(list **root, int v)
     }
     else {
         list *temp = find(root,v);
-
+        temp->next->prev = temp->prev;
+        temp->prev->next = temp->next;
+        free(temp);
     }
+}
+
+void showlist(list** root)
+{
+    while(!(*root)->istail){
+        printf("%d -> ", (*root)->data);
+        (*root) = (*root)->next;
+    }
+    printf("end\n");
 }
